af896e631027bf43c9ac303538b7500130107fcf
NikitaDombrovsky <65646953+NikitaDombrovsky@users.noreply.github.com>
2024-02-26 12:46:17 +0600

    Update App.kt

--- app/src/main/java/com/example/yamdemxmmapkt/App.kt
+++ app/src/main/java/com/example/yamdemxmmapkt/App.kt
@@ -9,6 +9,6 @@ class App : Application() {
 
         // Reading API key from BuildConfig.
         // Do not forget to add your MAPKIT_API_KEY property to local.properties file.
-        MapKitFactory.setApiKey("52eb0455-d7e5-4526-a6e4-ca5699d8a4bc")
+        MapKitFactory.setApiKey("YOUR_API_KEY")
     }
 }
